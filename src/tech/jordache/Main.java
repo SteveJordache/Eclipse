b9ac77b51181238ab171c75532f7a1d8e0423e06
root <root@stevej-ThinkPad>
2021-03-01 23:53:11 +0200

    new MOTD message

--- src/tech/jordache/Main.java
+++ src/tech/jordache/Main.java
@@ -3,5 +3,6 @@ package tech.jordache;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello Java is back in the city!!!");
+		System.out.println("Hello World 2021!");
 	}
 }
