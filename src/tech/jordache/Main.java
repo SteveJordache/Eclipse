a9c102e8ae0dc634baf21a19e570fcc257a3f707
root <root@stevej-ThinkPad>
2021-03-01 23:50:03 +0200

    first commit in feature-1 branch

--- src/tech/jordache/Main.java
+++ src/tech/jordache/Main.java
@@ -3,5 +3,8 @@ package tech.jordache;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello Java is back in the city!!!");
+		for (int i=0; i<100; i++) {
+			System.out.println("Halli Hallo ");
+		}
 	}
 }
